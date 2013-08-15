d71551fa6b7b433d77b0379fc81d50223b8ec82b
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2013-08-15 17:19:42 +0530

    Minor improvement
    Signed-off-by: Rocky Jaiswal <rocky.jaiswal@gmail.com>

--- lib/weatherman.rb
+++ lib/weatherman.rb
@@ -6,17 +6,20 @@ require 'weatherman/soap_wrapper'
 module Weatherman
   
   def self.get_cities_by_country(country)
-    soap_client = Weatherman::SOAPClient.new
-    parser = Weatherman::Parser.new
-    soap_wrapper = Weatherman::SOAPWrapper.new(soap_client, parser)
+    soap_wrapper = init()
     soap_wrapper.get_cities_by_country(country)
   end
 
   def self.get_weather(city, country)
+    soap_wrapper = init()
+    soap_wrapper.get_weather(city, country)
+  end
+
+  private
+  def self.init
     soap_client = Weatherman::SOAPClient.new
     parser = Weatherman::Parser.new
-    soap_wrapper = Weatherman::SOAPWrapper.new(soap_client, parser)
-    soap_wrapper.get_weather(city, country)
+    Weatherman::SOAPWrapper.new(soap_client, parser)
   end
 
 end
